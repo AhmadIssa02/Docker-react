9fdbf289a796f8978d3d6f256d31b6a6039f23c3
AhmadIssa02 <issa.ahmad2002@gmail.com>
2025-03-12 10:15:48 +0300

    birthday

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye There !!
-          hello
+          Happy Birthday! 🎉🥳
         </p>
         <a
           className="App-link"
